3f266fb555354053e12b0580892c9d839820f16d
Nishaharpreet <harpreetsood90@gmail.com>
2024-12-31 12:29:39 +0530

    add new

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-const dbHOST = process.env.DBHOST
+const dbHOST = process.env.DBHOST;
+
 mongoose.connect(dbHOST)
-  .then(()=>{
-    console.log('Connected!')
-   
-    app.listen(3000)
-  }).catch(()=> {
-console.log('Connection failed')
-  })
+    .then(() => {
+        console.log('MongoDB Connnected...')
+    }).catch((err) => {
+        console.log('Error while Mongo Conn..', err);
+    })
--- index.js
+++ index.js
@@ -1,32 +1,62 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
 const User = require('./model/user.model')
-require('dotenv').config()
+require('dotenv').config();
 require('./db');
-app.use(express.json());
-app.get('/', function (req, res) {
-  res.send('Hello World update server')
-})
+const PORT = process.env.PORT || 8080;
 
-app.get('/api/users', async (req, res)=> {
-  try {
-   const users =  await User.find({})
-   res.status(200).json(users)
-  }
-  catch(error) {
-      res.status(500).json({error: error.message})
-  }
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('First api running new deploy');
+});
+app.get('/ping', (req, res) => {
+    res.send('<=PONG=>');
+});
+app.get('/country', (req, res) => {
+    res.send('<=INDIA=>');
 });
 
+app.get('/ping', (req, res) => {
+    res.send('PONG')
+});
+// /products
 
-app.post('/api/users', async(req, res)=> {
-  try {
-      const data = await User.create(req.body)
-      res.status(200).json(data)
-  }
-  catch(error) {
-      res.status(500).json({error:error.message + 'dhanu'})
-  }
 
+app.use(express.json())
+app.get('/', (req, res)=> {
+    res.send("Hello We connected to mongo DB")
+  });
+
+//get users
+
+app.get('/api/users', async (req, res)=> {
+    try {
+     const users =  await User.find({})
+     res.status(200).json(users)
+    }
+    catch(error) {
+        res.status(500).json({error: error.message})
+    }
+});
+
+// single user product
+
+//product deleted API
+
+
+
+  app.post('/api/users', async(req, res)=> {
+    try {
+        const data = await User.create(req.body)
+        res.status(200).json(data)
+    }
+    catch(error) {
+        res.status(500).json({error:error.message + 'dhanu'})
+    }
+  
+  })
+
+app.listen(8080, () => {
+    console.log('Server is listenin on PORT :' + PORT);
 })
